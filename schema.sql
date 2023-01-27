6472655a2dcf12f41a29752020fab123f2234b63
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-01-27 00:05:02 -0800

    schema

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL 
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    role_title VARCHAR(30) NOT NULL,
+    salary DECIMAL() NOT NULL,
+    department_id INT ?
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT ?
+)
