12e9416ef7567c0cb4f6f3d3994d4457dc13f9e3
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-01-29 22:24:01 -0800

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,28 @@
 INSERT INTO department (department_name)
-VALUES ('techonology'), 
-       ('HR'),
+VALUES ('Sales'), 
+       ('Engineering'),
        ('Finance'),
-       ('Law');
-
-INSERT INTO role (role_title,salary, department_id)
-VALUES ('software engineer', 70000, 1),
-       ('HR manager', 60000, 2);
-
-
+       ('Legal');
+       
+INSERT INTO role (role_title, salary, department_id)
+VALUES ('Sales Lead', 100000, 1),
+       ('Salesperson', 80000, 1),
+       ('Lead Engineer', 150000, 2),
+       ('Software Engineer', 120000, 2),
+       ('Account Manager', 160000, 3),
+       ('Accountant', 125000, 3),
+       ('Legal Team Lead', 250000, 4),
+       ('Lawyer', 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('sharmaine', 'pineda', 1, NULL),
-       ('shar', 'pineda', 1, NULL),
-       ('maine', 'pineda', 1, 2),
-       ('S', 'p', 1, 1 );
+VALUES ('John', 'Doe', 1, NULL),
+       ('Mike', 'Chan', 2, 1),
+       ('Ashley', 'Rodriguez', 3, NULL),
+       ('Kevin', 'Tupik', 4, 3),
+       ('Kunal', 'Singh', 5, NULL),
+       ('Malia', 'Brown', 6, 5),
+       ('Sarah', 'Lourd', 7, NULL),
+       ('Tom', 'Allen', 8, 7);
        
 
 
